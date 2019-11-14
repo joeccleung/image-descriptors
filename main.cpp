9e23405876995a9a9be0fc159fb52f1d2c60b34d
joeccleung <joeccleung@gmail.com>
2019-11-14 08:34:24 -0800

    WIP: Read the images containing the image patches

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
-    return 0;
+   string pathToImages;
+
+   cout << "Please provide path to images for patches extraction" << endl;
+   cin >> pathToImages;
+
+
+   return 0;
 }
